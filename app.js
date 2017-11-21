372f791ec9a448054ce2ddeb9bdf007431a95719
liubanglong <liubanglong@ftrend.cn>
2017-11-21 13:48:55 +0800

    master ejs7 app.js tomcatdashboard.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 //--------------------程序所用模块声明（全在此处）------------------
-var express = require('express');
-var app = express();
-var favicon = require('serve-favicon');
-//var logger = require('morgan');//日志处理被log4js代替
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-var url = require("url");
-var formidable = require("formidable");//表单处理模块（包括文件上传）
-var querystring = require("querystring");//请求参数序列化和反序列化模块
-var node_xlsx = require('node-xlsx');//excel 存取模块
-var zip_local = require('zip-local');//文件压缩模块
-var ssh2 = require("ssh2");//ssh连接远程linux服务器模块
-var through = require('through');
-var events = require("events");
-var util = require("util");
-var session = require("express-session");//用来处理session
-var RedisStrore = require("connect-redis")(session);//用来处理session
-var async = require("async");//防止大量嵌套回调，流程处理模块
-var domain = require("domain");
-var hprose = require("hprose");//nodejs rpc模块（远程过程调用）
-var redis = require("redis");//redis存取模块
-var underscore = require("underscore");//对象、数组、函数处理工具模块
-var moment = require("moment");//时间、日期，获取、计算及格式化模块
-var https = require("https");//https请求
-var crypto = require("crypto");//签名模块
-var schedule = require("node-schedule");//定时执行模块
-var Sequelize = require("sequelize");//MySQL orm模块
+let express = require('express');
+let app = express();
+let favicon = require('serve-favicon');
+//let logger = require('morgan');//日志处理被log4js代替
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let path = require('path');
+let fs = require('fs');
+let url = require("url");
+let formidable = require("formidable");//表单处理模块（包括文件上传）
+let querystring = require("querystring");//请求参数序列化和反序列化模块
+let node_xlsx = require('node-xlsx');//excel 存取模块
+let zip_local = require('zip-local');//文件压缩模块
+let ssh2 = require("ssh2");//ssh连接远程linux服务器模块
+let through = require('through');
+let events = require("events");
+let util = require("util");
+let session = require("express-session");//用来处理session
+let RedisStrore = require("connect-redis")(session);//用来处理session
+let async = require("async");//防止大量嵌套回调，流程处理模块
+let domain = require("domain");
+let hprose = require("hprose");//nodejs rpc模块（远程过程调用）
+let redis = require("redis");//redis存取模块
+let underscore = require("underscore");//对象、数组、函数处理工具模块
+let moment = require("moment");//时间、日期，获取、计算及格式化模块
+let https = require("https");//https请求
+let crypto = require("crypto");//签名模块
+let schedule = require("node-schedule");//定时执行模块
+let Sequelize = require("sequelize");//MySQL orm模块
 //加载第三方模块
-var param = {
+let param = {
     app: app,
     fs: fs,
     path: path,
@@ -55,11 +55,11 @@ var param = {
 global["param"] = param;
 //---------------------------自定义模块----------------------------------
 //程序配置文件
-var myconfig = require("./config");
+let myconfig = require("./config");
 param["myconfig"] = myconfig;
 //初始化日志处理模块，静态文件放在日志处理之前，可避免输出静态文件访问日志
-var log4js = require("./service/logConfig");
-var logger = log4js.logger("production");//日志处理模块
+let log4js = require("./service/logConfig");
+let logger = log4js.logger("production");//日志处理模块
 param["logger"] = logger;
 log4js.configure();
 //初始化本程序运行依赖数据库
@@ -97,7 +97,7 @@ process.on('uncaughtException', function (err) {
 //为所有的req,res做错误处理（内部逻辑未捕获错误，全局由此处理）
 app.use(function (req, res, next) {
     //监听domain的错误事件
-    var mydomain = domain.create();
+    let mydomain = domain.create();
     mydomain.on('error', function (err) {
         mydomain.dispose();
         console.log(err);
@@ -113,7 +113,7 @@ app.use(function (req, res, next) {
 //设置访问权限，未登录只能访问指定文件夹中的内容（config.unRegister）,登录超时，ajax请求确保跳转登录页
 app.use(function (req, res, next) {
     //如果是公共路径直接放行
-    var rootPath = url.parse(req.url).pathname.split("/")[1];
+    let rootPath = url.parse(req.url).pathname.split("/")[1];
     if (underscore._.indexOf(myconfig.unRegister, rootPath) >= 0) {
         next();
         return;
@@ -132,8 +132,8 @@ app.use(function (req, res, next) {
 
     }
     //如果已经登录，此时判断请求路径，当前用户是否有权限
-    var authority = param.authority;
-    var req_pathname = url.parse(req.url).pathname;
+    let authority = param.authority;
+    let req_pathname = url.parse(req.url).pathname;
     //后端判断当前用户访问路径权限 最好把用户权限放到redis中，否则每一次请求都会查询数据库
     authority.isUserOperatorPass(req_pathname, req, function (err, req_authority) {
         //有权限放行
@@ -164,7 +164,7 @@ app.use("/", function (req, res) {
 //--------express框架错误处理，设置友好错误界面（没用到）--------
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
--- routes/tomcat/tomcatDashboard.js
+++ routes/tomcat/tomcatDashboard.js
@@ -3,17 +3,14 @@
  */
 //tomcat部署
 module.exports = function (param, routeDir) {
-    var express = require("express");
-    var router = express.Router();
-    var path = require('path');
-    var fs = require('fs');
-    var logError = param.publicmethod.logError;
-    var publicmethod = param.publicmethod;
-    var authority = param.authority;
-    var _ = param.underscore._;
-    var utils = param.utils;
-    var execRpcCmd = function (app_server_id, env_name, action) {
-        console.log(app_server_id, env_name);
+    let express = require("express");
+    let router = express.Router();
+    let logError = param.publicmethod.logError;
+    let publicmethod = param.publicmethod;
+    let authority = param.authority;
+    let _ = param.underscore._;
+    let utils = param.utils;
+    let execRpcCmd = function (app_server_id, env_name, action) {
         return Promise.all([
             deploy_app_server.findOne({
                 where: {
@@ -30,30 +27,112 @@ module.exports = function (param, routeDir) {
                 attributes: ["tunnel_ip"]
             })
         ]).then(function (data) {
-            var host_info = data[0];
-            var env_info = data[1];
+            let host_info = data[0];
+            let env_info = data[1];
             if (!host_info || !env_info) {
                 return Promise.reject("没有查到程序环境或服务器信息");
             }
-            var outaddr = "http://" + env_info["tunnel_ip"] + ":3002";
-            var inneraddr = "http://" + host_info["host_ip"] + ":" + host_info["map_rpc_port"];
+            let outaddr = "http://" + env_info["tunnel_ip"] + ":3002";
+            let inneraddr = "http://" + host_info["host_ip"] + ":" + host_info["map_rpc_port"];
             return publicmethod.execRpcServiceProxy(outaddr, inneraddr, action, []);
         });
     };
+    let listFunc = async function (req, form_fields) {
+        let env_name = form_fields["env_name"];
+        let user_id = req.session.user["user_id"] || 0;
+        if (!_.has(form_fields, "env_name")) {
+            return Promise.reject("参数缺失");
+        }
+        let deploy_res_info_where = {env_name: env_name};
+        if (_.has(form_fields, "search_condition")) {
+            deploy_res_info_where["$or"] = {
+                group_name: {$like: "%" + form_fields["search_condition"] + "%"},
+                program_name: {$like: "%" + form_fields["search_condition"] + "%"}
+            };
+        }
+        let server_info = await deploy_res_info.findAndCountAll({
+            include: [{
+                model: deploy_app_server,
+                required: true,
+                where: {
+                    is_disable: 0
+                },
+                attributes: [["id", "app_server_id"], "alias", "host_ip", "map_rpc_port"]
+            }, {
+                model: deploy_user_res,
+                required: true,
+                attributes: [[Sequelize.fn("GROUP_CONCAT", Sequelize.col("privilege_code")), "privilege_code"]],
+                where: {
+                    user_id: user_id
+                }
+            }, {
+                model: deploy_env_config,
+                required: true,
+                attributes: ["tunnel_ip"]
+            }],
+            attributes: ["env_name", "group_name", "program_name"],
+            where: deploy_res_info_where,
+            limit: Number(form_fields["limit"]) || 0,
+            offset: Number(form_fields["offset"]) || 0,
+            group: [[deploy_app_server, "id"]],
+            raw: true
+        });
+        let total = server_info["count"].length;
+        let server = Sequelize.getPlaneData(server_info["rows"]);
+        //获取运行状态
+        for (let i = 0, j = server.length; i < j; i++) {
+            let outaddr = /http:\/\//.test(server[i]["tunnel_ip"]) ? server[i]["tunnel_ip"] + ":3002" : "http://" + server[i]["tunnel_ip"] + ":3002";
+            let inneraddr = /http:\/\//.test(server[i]["host_ip"]) ? server[i]["host_ip"] + ":" + server[i]["map_rpc_port"] : "http://" + server[i]["host_ip"] + ":" + server[i]["map_rpc_port"];
+            server[i]["run_status"] = await publicmethod.execRpcServiceProxy(outaddr, inneraddr, "getTomcatStatus", []);
+        }
+        //获取最后一次部署
+        let app_server_ids = _.pluck(server, "app_server_id");
+        let deploy_history = await deploy_app_history.findAll({
+            where: {
+                app_server_id: {$in: app_server_ids}
+            },
+            attributes: [[Sequelize.fn("max", Sequelize.col("id")), "max_id"]],
+            group: ["app_server_id"],
+            raw: true
+        });
+        let ids = _.pluck(deploy_history, "max_id");
+        let history = await deploy_app_history.findAll({
+            where: {
+                id: {$in: ids}
+            },
+            attributes: ["app_server_id", "deploy_time", "apptype", "appversion", "user_name", "deploy_result"],
+            raw: true
+        });
+        //合并最后一次部署
+        let _server = {};
+        let _history = {};
+        //隐蔽ip和端口信息
+        _.each(server, function (item) {
+            _server[item["app_server_id"]] = _.omit(item, "host_ip", "map_rpc_port", "tunnel_ip");
+        });
+        _.each(history, function (item) {
+            _history[item["app_server_id"]] = item;
+        });
+        for (let app_server_id in _server) {
+            _.extend(_server[app_server_id], _history[app_server_id]);
+        }
+        let rows = _.chain(_server).values().sortBy("group_name", "program_name", "alias");
+        return Promise.resolve({"total": total, "rows": rows});
+    };
     router.get("/getTomcatDashboard", function (req, res, next) {
-        var form_data = utils.getForm(req);
-        var form_fields = form_data["fields"];
-        var sider_id = form_fields["sider_id"] || 0;
-        var env_name = form_fields["env_name"];
+        let form_data = utils.getForm(req);
+        let form_fields = form_data["fields"];
+        let sider_id = form_fields["sider_id"] || 0;
+        let env_name = form_fields["env_name"];
         Promise.all([
             //菜单栏
             authority.getUserSiderMenu(req),
             authority.getUserOperator(req, sider_id),
             authority.getUserEnvResource(req, 12)
         ]).then(function (data) {
-            var menu = data[0];
-            var operator = data[1];
-            var envs = data[2];
+            let menu = data[0];
+            let operator = data[1];
+            let envs = data[2];
             res.render(routeDir + "tomcatDashboard/index", {
                 envs: envs,
                 operator: operator,
@@ -67,109 +146,16 @@ module.exports = function (param, routeDir) {
         });
     });
     router.get('/:action', function (req, res, next) {
-        var action = req.params.action;
-        var form_data = utils.getForm(req);
-        var form_fields = form_data["fields"];
+        let action = req.params.action;
+        let form_data = utils.getForm(req);
+        let form_fields = form_data["fields"];
         switch (action) {
             case "list": {
-                var env_name = form_fields["env_name"];
-                var user_id = req.session.user["user_id"] || 0;
-                if (!_.has(form_fields, "env_name")) {
-                    res.end(JSON.stringify({"status": "error", "msg": "参数缺失"}));
-                    return;
-                }
-                var total = 0, rows = [];
-                var deploy_res_info_where = {env_name: env_name};
-                if (_.has(form_fields, "search_condition")) {
-                    deploy_res_info_where["$or"] = {
-                        group_name: {$like: "%" + form_fields["search_condition"] + "%"},
-                        program_name: {$like: "%" + form_fields["search_condition"] + "%"}
-                    };
-                }
-                deploy_res_info.findAndCountAll({
-                    include: [{
-                        model: deploy_app_server,
-                        required: true,
-                        where: {
-                            is_disable: 0
-                        },
-                        attributes: [["id", "app_server_id"], "alias", "host_ip", "map_rpc_port"]
-                    }, {
-                        model: deploy_user_res,
-                        required: true,
-                        attributes: [[Sequelize.fn("GROUP_CONCAT", Sequelize.col("privilege_code")), "privilege_code"]],
-                        where: {
-                            user_id: user_id
-                        }
-                    }, {
-                        model: deploy_env_config,
-                        required: true,
-                        attributes: ["tunnel_ip"]
-                    }],
-                    attributes: ["env_name", "group_name", "program_name"],
-                    where: deploy_res_info_where,
-                    limit: Number(form_fields["limit"]) || 0,
-                    offset: Number(form_fields["offset"]) || 0,
-                    group: [[deploy_app_server, "id"]],
-                    raw: true
-                }).then(function (data) {
-                    total = data["count"].length;
-                    rows = Sequelize.getPlaneData(data["rows"]);
-                    //获取运行状态
-                    var Promi_all = [];
-                    //异步操作
-                    for (var i = 0, j = rows.length; i < j; i++) {
-                        var outaddr = /http:\/\//.test(rows[i]["tunnel_ip"]) ? rows[i]["tunnel_ip"] + ":3002" : "http://" + rows[i]["tunnel_ip"] + ":3002";
-                        var inneraddr = /http:\/\//.test(rows[i]["host_ip"]) ? rows[i]["host_ip"] + ":" + rows[i]["map_rpc_port"] : "http://" + rows[i]["host_ip"] + ":" + rows[i]["map_rpc_port"];
-                        Promi_all.push(publicmethod.execRpcServiceProxy(outaddr, inneraddr, "getTomcatStatus", []));
-                    }
-                    return Promise.all(Promi_all).then(function (data) {
-                        _.each(data, function (item, index) {
-                            rows[index]["run_status"] = item;
-                        });
-                        return Promise.resolve();
-                    });
-                }).then(function (data) {
-                    //获取最后一次部署
-                    var app_server_ids = _.pluck(rows, "app_server_id");
-                    return deploy_app_history.findAll({
-                        where: {
-                            app_server_id: {$in: app_server_ids}
-                        },
-                        attributes: [[Sequelize.fn("max", Sequelize.col("id")), "max_id"]],
-                        group: ["app_server_id"],
-                        raw: true
-                    }).then(function (data) {
-                        var ids = _.pluck(data, "max_id");
-                        return deploy_app_history.findAll({
-                            where: {
-                                id: {$in: ids}
-                            },
-                            attributes: ["app_server_id", "deploy_time", "apptype", "appversion", "user_name", "deploy_result"],
-                            raw: true
-                        }).then(function (data) {
-                            //合并最后一次部署
-                            var _rows = {};
-                            var _data = {};
-                            //隐蔽ip和端口信息
-                            _.each(rows, function (item) {
-                                _rows[item["app_server_id"]] = _.omit(item, "host_ip", "map_rpc_port", "tunnel_ip");
-                            });
-                            _.each(data, function (item) {
-                                _data[item["app_server_id"]] = item;
-                            });
-                            for (var app_server_id in _rows) {
-                                _.extend(_rows[app_server_id], _data[app_server_id]);
-                            }
-                            rows = _.chain(_rows).values().sortBy("group_name", "program_name", "alias");
-                            return Promise.resolve();
-                        });
-                    })
-                }).then(function () {
+                listFunc(req, form_fields).then(function (data) {
                     res.end(JSON.stringify({
                         "status": "success",
                         "msg": "",
-                        "data": {"total": total, "rows": rows}
+                        "data": data
                     }));
                 }).catch(function (err) {
                     res.end(JSON.stringify({"status": "error", "msg": logError(err)}));
