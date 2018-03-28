1da82b43cc54981a755902550f207479c316b4d8
liubanglong <liubanglong@smartpos.top>
2018-03-28 18:08:56 +0800

    asdf

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
-var _=require("underscore")._;
-var asd=undefined;
-console.log(_.isEmpty(asd));
+// let str = '[1,2,3,4]';
+// let str = '{"name":1,"age":2}';
+let str = "123";
+let _obj = JSON.parse(str);
+console.log(_obj);
